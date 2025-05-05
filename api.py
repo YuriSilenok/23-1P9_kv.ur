fd4fd4a3f947421a6c400b86c46f728797e31927
Юрий Силенок <yuri.silenok@gmail.com>
2025-05-05 16:35:20 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,11 @@
 """FastAPI к квадратному уравнению"""
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from logic import kv_ur
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="public"))
 
 @app.get("/kv_ur")
 def kv_ur_get(a, b, c):
