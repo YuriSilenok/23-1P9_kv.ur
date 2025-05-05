166cc39ed64d9ae1e5e374446e50e76a3b2e39a5
Nikita Rumyantsev <nikita.ded0000@gmail.com>
2025-05-05 17:46:15 +0300

    Добавили JS код для вычисления дискриминанта

--- app.py
+++
@@ -1,8 +0,0 @@
-"""Отправка html-страницы при помощи FastAPI"""
-
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="public"))
