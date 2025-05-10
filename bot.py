456c1369edf458190252d408a91db8da22f570ee
Кулаков Никита <bbulb46@gmail.com>
2025-05-10 09:16:10 +0300

    Добавление телеграм бота, исправление ошибок

--- bot.py
+++ bot.py
@@ -1,39 +1,45 @@
-"""Модули для работы с aiogram"""
+"""Импорт нужных модулей"""
 import asyncio
+import os
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from dotenv import load_dotenv
 from logic import kv_ur
 
-TOKEN = ""
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
 
 
+@dp.message(CommandStart())
 async def start(message: Message):
     """Функция приветствия"""
     await message.answer(
-        "Пришлите три числа a b c через пробел, например: 1 -5 6")
+        "Пришлите три числа a b c через пробел, например: 1 -5 6"
+    )
 
 
+@dp.message()
 async def numbers(message: Message):
-    """Функция работы самой функции"""
-    parts = message.text.replace(",", " ").split()
+    """Разбираем ввод и отвечаем результатом kv_ur"""
+    parts = message.text.split()
     if len(parts) != 3:
         return await message.answer("Нужно ровно три числа")
+
     try:
         a, b, c = map(float, parts)
     except ValueError:
         return await message.answer("Все три значения должны быть числами.")
-    await message.answer("\n".join(map(str, kv_ur(a, b, c))))
 
-# ─── инициализация ───
-bot = Bot(TOKEN)
-dp = Dispatcher()
-dp.message(CommandStart())(start)
-dp.message()(numbers)
+    await message.answer("\n".join(map(str, kv_ur(a, b, c))))
 
 
 async def main():
-    """Функция старта бота"""
+    """Функция запуска бота"""
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
