bac40d57c118d3ed6e6f6716c6dbdaddabf030fb
OrlovMatvey <deceptor27@mail.ru>
2025-05-13 18:04:14 +0300

    Реализовал интерфейс решения квадратного уравнения на tkinter. С помощью класса.

--- desktop.py
+++ desktop.py
@@ -3,47 +3,54 @@ import tkinter as tk
 from logic import kv_ur
 
 
-root = tk.Tk()
-root.title("Решение квадратного уравнения")
+class Kvurtkint(tk.Tk):
+    """Класс для создания окна программы"""
+    def __init__(self):
+        super().__init__()
+        self.title("Решение квадратного уравнения")
 
-lab_1 = tk.Label(width=85, text='Введите значения a,b,c')
+        self.lab_1 = tk.Label(self, width=85, text='Введите значения a,b,c')
 
-ent_1 = tk.Entry(width=100)
+        self.ent_1 = tk.Entry(self, width=100)
 
-ent_2 = tk.Entry(width=100)
+        self.ent_2 = tk.Entry(self, width=100)
 
-ent_3 = tk.Entry(width=100)
+        self.ent_3 = tk.Entry(self, width=100)
 
-but = tk.Button(width=85, text='Вычислить', bg='black', fg='yellow')
+        self.but = tk.Button(
+            self, width=85,
+            text='Вычислить', bg='black', fg='yellow', command=self.vichisl
+            )
 
-lab_2 = tk.Label(width=85, text='Ваш ответ:')
+        self.lab_2 = tk.Label(self, width=85, text='Ваш ответ:')
 
-lab_3 = tk.Label(width=86, bg='black', fg='yellow')
+        self.lab_3 = tk.Label(self, width=86, bg='black', fg='yellow')
+
+        self.but.bind(self, '<Button-1>', self.vichisl)
+
+        self.lab_1.pack()
+
+        self.ent_1.pack()
+
+        self.ent_2.pack()
+
+        self.ent_3.pack()
+
+        self.but.pack()
+
+        self.lab_2.pack()
+
+        self.lab_3.pack()
+
+    def vichisl(self):
+        """Функция для вычислений"""
+        a = int(self.ent_1.get())
+        b = int(self.ent_2.get())
+        c = int(self.ent_3.get())
+        kv_ur(a, b, c)
+        self.lab_3['text'] = kv_ur(a, b, c)
 
 
-def vichisl(event):
-    """fdasf"""
-    a = int(ent_1.get())
-    b = int(ent_2.get())
-    c = int(ent_3.get())
-    kv_ur(a, b, c)
-    lab_3['text'] = kv_ur(a, b, c)
-
-
-but.bind('<Button-1>', vichisl)
-
-lab_1.pack()
-
-ent_1.pack()
-
-ent_2.pack()
-
-ent_3.pack()
-
-but.pack()
-
-lab_2.pack()
-
-lab_3.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    app = Kvurtkint()
+    app.mainloop()
